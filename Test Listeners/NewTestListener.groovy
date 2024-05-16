6070b7438207cdf02ab49b3bbaf8cc4f57f80c8c
VedantKumarSharma <vsharma@kognivera.com>
2024-05-16 13:09:48 +0530

    update
    Removed GR objects

--- Test Listeners/NewTestListener.groovy
+++ Test Listeners/NewTestListener.groovy
@@ -1,4 +1,4 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+/*import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
@@ -46,3 +46,4 @@ class NewTestListener {
 		WebUI.closeBrowser('')
 	}
 }
+*/
